7bf6000666ee1fa0516c47e4ea92369f803e0c2c
Hugh Chambers <hugh.chambers@student.uts.edu.au>
2023-08-30 15:45:48 +1000

    Added in skeleton for init functions and general init structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,15 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
+  /*
+  Run all of the initialisation functions
+  If one of the functions fails to initialise,
+  flag it and beep out an error code at the end of setup.
+  Re-run setup until we pass initialisation without any errors
+  
+  If initialisation occurs without any problems, then proceed into the main loop
+  */
 }
 
 void loop() {
@@ -16,3 +24,39 @@ void loop() {
 int myFunction(int x, int y) {
   return x + y;
 }
+
+
+
+
+/*
+-----------------------FUNCTION PLAN-------------------------------
+
+-----------------------VOID_SETUP------------------------------
+
+_______________________GENERAL INITIALISATION______________________
+Setup serial
+anything miscellaneous relating to setup
+
+_______________________BARO INITIALISATION______________________
+configure barometer settings
+initialise barometer
+
+_______________________SERVO INITIALISATION______________________
+configure servo settings
+initialise servo
+
+_______________________BUZZER INITIALISATION______________________
+configure buzzer settings
+initialise buzzer
+
+_______________________ROTARY SWITCH INITIALISATION______________________
+configure rotary switch settings
+initialise rotary switch
+
+_______________________EEPROM INITIALISATION______________________
+Configure eeprom settings
+initialise eeprom
+
+
+
+*/
